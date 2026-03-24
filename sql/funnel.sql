a38b7d5fad9ebb159f0f9bda9d9b8461168d9a3a
constantinepyro666 <constantine.pyro666@gmail.com>
2026-03-24 12:27:47 +0500

    Update funnel.sql

--- sql/funnel.sql
+++ sql/funnel.sql
@@ -1,5 +1,8 @@
 SELECT 
-	COUNT(DISTINCT CASE WHEN event_type = 'signup' THEN user_id END) as signup,
-    COUNT(DISTINCT CASE WHEN event_type = 'login' THEN user_id END) as login,
-	COUNT(DISTINCT CASE WHEN event_type = 'create_note' THEN user_id END) as create_note
+	--COUNT(DISTINCT CASE WHEN event_type = 'signup' THEN user_id END) as signup,
+    --COUNT(DISTINCT CASE WHEN event_type = 'login' THEN user_id END) as login,
+	--COUNT(DISTINCT CASE WHEN event_type = 'create_note' THEN user_id END) as create_note
+	COUNT(DISTINCT user_id) AS users
 FROM events
+GROUP BY event_yype
+ORDER BY users DASC
