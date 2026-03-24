3f53c2aab3e6e23e169550e7c019e802f09dc94b
constantinepyro666 <constantine.pyro666@gmail.com>
2026-03-24 12:36:37 +0500

    Update funnel.sql

--- sql/funnel.sql
+++ sql/funnel.sql
@@ -2,6 +2,7 @@ SELECT
 	--COUNT(DISTINCT CASE WHEN event_type = 'signup' THEN user_id END) as signup,
     --COUNT(DISTINCT CASE WHEN event_type = 'login' THEN user_id END) as login,
 	--COUNT(DISTINCT CASE WHEN event_type = 'create_note' THEN user_id END) as create_note
+	event_type,
 	COUNT(DISTINCT user_id) AS users
 FROM events
 GROUP BY event_type
