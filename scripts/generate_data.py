bee685493284f44fa17311934f5c0f416bbedd7d
constantinepyro666 <constantine.pyro666@gmail.com>
2026-04-14 14:15:18 +0500

    Update generate_data.py

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -10,20 +10,28 @@ events = []
 
 start_date = datetime(2026, 2, 1)
 
+platform = random.choices(
+    ["iOS", "Android", "Web"],
+    weights=[0.4, 0.4, 0.2]
+)[0]
+
 for user_id in range(1, NUM_USERS + 1):
+
+    platform = random.choice(platforms)
+    
     # login (всегда)
     login_time = start_date + timedelta(days=random.randint(0, 30))
-    events.append([user_id, "login", login_time, "web"])
+    events.append([user_id, "login", login_time, platform])
 
     # view_note
     if random.random() < 0.7:
         view_time = login_time + timedelta(days=random.randint(0, 5))
-        events.append([user_id, "view_note", view_time, "web"])
+        events.append([user_id, "view_note", view_time, platform])
 
     # create_note
     if random.random() < 0.4:
         create_time = login_time + timedelta(days=random.randint(0, 5))
-        events.append([user_id, "create_note", create_time, "web"])
+        events.append([user_id, "create_note", create_time, platform])
 
 df = pd.DataFrame(events, columns=["user_id", "event_type", "event_time", "platform"])
 
