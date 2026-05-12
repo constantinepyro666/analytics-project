95b6fd491e6a0f867a9afd7394210bfbca23a8af
constantinepyro666 <constantine.pyro666@gmail.com>
2026-05-12 14:19:53 +0500

    Update funnel.sql

--- sql/funnel.sql
+++ sql/funnel.sql
@@ -1,12 +1,7 @@
---SELECT event_type, COUNT(DISTINCT user_id) as users
---FROM events
---GROUP BY event_type;
-SELECT event_type, COUNT(DISTINCT user_id) AS users
-FROM events
-GROUP BY event_type
-ORDER BY 
-    CASE event_type
-        WHEN 'login' THEN 1
-        WHEN 'view_note' THEN 2
-        WHEN 'create_note' THEN 3
-    END;
+SELECT 
+    platform,
+    COUNT(DISTINCT user_id) FILTER (WHERE event_type = 'login') as login,
+    COUNT(DISTINCT user_id) FILTER (WHERE event_type = 'view_note') as view_note,
+    COUNT(DISTINCT user_id) FILTER (WHERE event_type = 'create_note') as create_note
+FROM user_events
+GROUP BY platform;
